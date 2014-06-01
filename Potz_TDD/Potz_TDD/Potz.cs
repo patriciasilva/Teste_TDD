9dcdc3629c0124c80ad67ffc598c9805389d5a4c
Patricia Silva <patriciavirgilio1@gmail.com>
2014-06-01 16:46:59 -0300

    Is valido

---
+++ Potz_TDD/Potz_TDD/Potz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Potz_TDD
+{
+    public class Potz
+    {
+        public bool isValido()
+        {
+
+            return true;
+        }
+    }
+}
