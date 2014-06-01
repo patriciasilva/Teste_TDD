f59d4a3b847fc4904bf50304fa66070d46a6f846
Patricia Silva <patriciavirgilio1@gmail.com>
2014-06-01 18:45:47 -0300

    Is valido

---
+++ Potz_TDD/Potz_TDD/CumpomPotz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Potz_TDD
+{
+    public class CupomPotz
+    {
+        private string numeroCupom;
+        private int qtdePotz;
+
+        public CupomPotz(string numeroCupom)
+        {
+            this.numeroCupom = numeroCupom.Replace("-", "");
+            this.qtdePotz = Int32.Parse(numeroCupom.Substring(0,3));
+        }
+
+        public int getPotz()
+        {
+            return qtdePotz;
+        }
+
+        public bool isValido()
+        {
+            int total = 0;
+            int i = 1;
+            for (int j = numeroCupom.Length - 1; j > 0; j--)
+            {
+                //Substring na posição j e a quantidade de caracteres que é igual a 1
+                total = total + (Int32.Parse(numeroCupom.Substring(j, 1)) * i);
+                i++;
+            }
+            return (total % 11) == 0;
+        }
+    }
+}
---
+++ Potz_TDD/TestPotz/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Potz_TDD;
+
+namespace TestPotz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsValidoTestTrue()
+        {
+            CupomPotz cupompotz = new CupomPotz("500036532-7");
+
+            Assert.IsTrue(cupompotz.isValido());
+        }
+
+        [TestMethod]
+        public void IsValidoTestFalse()
+        {
+            CupomPotz cupompotz = new CupomPotz("100036532-7");
+
+            Assert.IsFalse(cupompotz.isValido());
+        }
+    }
+}
